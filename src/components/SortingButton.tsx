36f73252c059d148793fd166befb72f238b52156
corneliaDev <cmueller.mail@t-online.de>
2024-12-07 10:59:00 +0100

    hover state

--- src/components/SortingButton.tsx
+++ src/components/SortingButton.tsx
@@ -13,9 +13,9 @@ export default function SortingButton({
 }: SortingButtonProps) {
   return (
     <button
-      className={clsx("font-semibold", {
-        "text-brightBlue hover:font-bold": text === "All",
-        "text-lightGray hover:text-lTVeryDarkGrayishBlue dark:hover:text-lTVeryLightGrayishBlue":
+      className={clsx("font-semibold hover:font-bold active:font-bold", {
+        "text-brightBlue": text === "All",
+        "text-lightGray hover:text-lTVeryDarkGrayishBlue active: text-lTVeryDarkGrayishBlue dark:hover:text-lTVeryLightGrayishBlue":
           text === "Active" || text === "Completed",
       })}
       onClick={onClick}
