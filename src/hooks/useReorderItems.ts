be018eda2d0d4d3aa6423215c910485de6130960
corneliaDev <cmueller.mail@t-online.de>
2024-12-07 10:06:53 +0100

    inital items

--- src/hooks/useReorderItems.ts
+++ src/hooks/useReorderItems.ts
@@ -4,9 +4,10 @@ import { ToDoItem } from "../lib/types";
 
 export default function useReorderItems() {
   const toDoItems = usetodoAppStore((state) => state.listItems);
+  const initialItems = usetodoAppStore((state) => state.items);
 
   // state for framer-motion Reorder component
-  const [listItems, setListItems] = useState<ToDoItem[]>([]);
+  const [listItems, setListItems] = useState<ToDoItem[]>(initialItems);
 
   useEffect(() => {
     if (!toDoItems) return;
